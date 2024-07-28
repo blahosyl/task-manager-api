7afe55e77dd2884a4d3259450f71253adb3bed61
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-28 18:47:05 +0000

    add Task model & migrate

--- README.md
+++ README.md
@@ -14,6 +14,10 @@
 
 ##### Task model
 
+Tasks should remain intact even when their creator (author) or assignee are deleted (e.g., when a team memeber leaves an organization). In this case, the respective fields for the deleted user are set to `null`. Accordingly, the `author` and `assignee` fields are allowed to be `null`.
+
+##### Watch model
+
 #### Access management structure
 
 #### CRUD
@@ -122,9 +126,18 @@
 
 ### Code credits
 
+This project was developed on the basis of the [DRF-API](https://github.com/Code-Institute-Solutions/drf-api) by [Code Institute](https://github.com/Code-Institute-Solutions/).
+
+I have also consulted the project [Tick It](https://github.com/Code-Institute-Submissions/ci_pp5_tick_it_react) by [Jamie King](https://github.com/jkingportfolio).
+
+Front End based on [Moments](https://github.com/Code-Institute-Solutions/moments0) by [Code Institute](https://github.com/Code-Institute-Solutions/).
+
 ### Related advice
 
 ### Study/lookup sources
+
+- [Django model field reference](https://docs.djangoproject.com/en/3.2/ref/models/fields/)
+- [Django `on_delete` options](https://www.queworx.com/django/django-on_delete/)
 
 ### Text
 
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    """
+    Task model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
+    PRIORITY_OPTIONS = [
+        ('HIGH', 'High'),
+        ('MEDIUM', 'Medium'),
+        ('LOW', 'Low'),
+    ]
+
+    STATUS_OPTIONS = [
+        ('TO-DO', 'To do'),
+        ('IN-PROGRESS', 'In progress'),
+        ('COMPLETED', 'CompleteD'),
+    ]
+
+    author = models.ForeignKey(User, blank=True,
+                                    null=True,
+                                    on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    excerpt = models.CharField(max_length=1000)
+    description = models.TextField(blank=True)
+    assignee = models.ForeignKey(User, blank=True,
+                                    null=True,
+                                    on_delete=models.SET_NULL,
+                                    related_name='assignee')
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_w2n3ng', blank=True
+    )
+    priority = models.CharField(max_length=255,
+                                choices=PRIORITY_OPTIONS,
+                                default='LOW')
+    status = models.CharField(max_length=255,
+                                 choices=STATUS_OPTIONS,
+                                 default='TO-DO')
+    due_date = models.DateTimeField(blank=True, null=True)
+
+
+    
+    class Meta:
+        ordering = ['-created_at']
+
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
