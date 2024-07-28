f8196e68ab28d947aa39f6527af988ae843623e5
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-28 19:40:43 +0000

    add task serialiers, task list view & url #7

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, generics
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskList(generics.ListCreateAPIView):
+    """
+    List tasks or create a task if logged in
+    The perform_create method associates the task with the logged in user.
+    """
+    serializer_class = TaskSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    queryset = Task.objects.all().order_by('-created_at')
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
