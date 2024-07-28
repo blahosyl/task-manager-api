5773190000a842f5e1355f82dea87a47d53eaac4
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-28 15:28:42 +0000

    create profile model & register it in admin #6
    - automatically create a profile when a new user is registered
    - makemigrations
    - migrate

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    firstname = models.TextField(max_length=255, blank=True)
+    lastname = models.TextField(max_length=255, blank=True)
+    pronouns = models.TextField(max_length=255, blank=True)
+    role = models.TextField(max_length=255, blank=True)
+    about = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_bfkzff'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+# automatically create profile model every time a user is created
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
