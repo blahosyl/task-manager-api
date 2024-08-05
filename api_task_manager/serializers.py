27575ef3997240bc314248953b871441a351b395
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-08-05 15:00:49 +0000

    add profile_firstname and _lastname to serializer #75

--- api_task_manager/serializers.py
+++ api_task_manager/serializers.py
@@ -4,12 +4,16 @@ from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
     """
-    Get profile ID and profile image of current user
+    Get profile ID, firstname, lastname and profile image of current user
     """
     profile_id = serializers.ReadOnlyField(source='profile.id')
+    profile_fistname = serializers.ReadOnlyField(
+        source='profile.profile_fistname')
+    profile_lastname = serializers.ReadOnlyField(source='profile.lastname')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
+            'profile_id', ' profile_fistname', ' profile_lastname', 
+            'profile_image'
         )
