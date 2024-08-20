5dbf718dfc940b773eeefc2290b959a9e4475bbe
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-08-20 01:28:19 +0000

    set Debug to False before deployment #28

--- api_task_manager/settings.py
+++ api_task_manager/settings.py
@@ -73,9 +73,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# get allowe hosts from the environment
+# get allow hosts from the environment
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'),]
 
 # allowed frontend apps
